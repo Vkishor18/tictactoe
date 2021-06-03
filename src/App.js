c00e35f019de55764e8fc853dd2431b19f110f2e
vkishor18 <vanshikakishor00@gmail.com>
2021-06-04 02:27:14 +0530

    added Board State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Board from './components/Board';
 import './styles/root.scss';
+import { useState } from 'react';
 const App = () =>
 {
+  
   return (
-    <div className = "app" >
+  <div className = "app" >
     <h1>TIC TAC TOE </h1>
     <Board />
   </div>
